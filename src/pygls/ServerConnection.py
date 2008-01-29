09d9d181e28ec3f44a8d18abf6ead592b26ff8e0
kichkasch <kichkasch@gmx.de>
2008-01-29 17:45:33 +0000

    git-svn-id: http://tools.assembla.com/svn/om_gpslocationsharing/client/Python/trunk@82 f5e22e0a-0f59-dc11-ae45-00188b745eb1

---
+++ src/pygls/ServerConnection.py
@@ -0,0 +1,11 @@
+"""
+Python library for GPS Location Sharing.
+http://www.assembla.com/wiki/show/dZdDzazrmr3k7AabIlDkbG
+
+This module is the entry point for the connection to the GLS Server.
+
+@author: Michael Pilgermann
+@contact: mailto:michael.pilgermann@gmx.de
+@contact: http://www.kichkasch.de
+@license: GPL (General Public License)
+"""
